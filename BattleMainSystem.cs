8ef25595c83744be8848af4497380dbe46e44fe8
isodadosi <isodadosi@gmail.com>
2020-08-02 07:53:28 +0900

    完成まで

--- BattleMainSystem.cs
+++ BattleMainSystem.cs
@@ -7,18 +7,37 @@ public class BattleMainSystem : MonoBehaviour
     public Unit player;
     public Unit enemy;
     public GameObject ResultPanel;
+    public GameObject PlayerPanel;
+    public GameObject PlayerChimeraPanel;
+
+    private GameState currentState;
 
     bool IsPlayerTurn;
     bool IsGameOver;
+    bool CommandSelect;
 
     float second = 0f;
+
+    private enum GameState
+    {
+        PlayerSelect, //#プレイヤーのコマンド入力
+        PlayerChimeraSelect, //#プレイヤーのキメラのコマンド入力
+        PlayerTurnEnd, //#プレイヤーの選択が終わり、処理を実行
+        EnemySelect, //#敵のコマンド入力
+        EnemyChimeraSelect, //#敵のキメラのコマンド入力
+        EnemyTurnEnd, //#敵の選択が終わり、処理を実行
+        GameWin, //#敵のHPがなくなった場合
+        GameOver, //#自分のHPがなくなった場合
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        IsPlayerTurn = true;
-        IsGameOver = false;
+        currentState = GameState.PlayerSelect;
+        PlayerPanel.SetActive(true);
+        PlayerChimeraPanel.SetActive(false);
         ResultPanel.SetActive(false);
+        CommandSelect = false;
     }
 
     // Update is called once per frame
@@ -28,36 +47,126 @@ public class BattleMainSystem : MonoBehaviour
         ResultPanel.SetActive(true);
     }
 
-    void Update()
+
+
+
+    //ターン状態の遷移
+
+    public void PlayerSelect()
     {
-        if (player.hp == 0 || enemy.hp == 0)
+        if(CommandSelect == true)
         {
-            IsGameOver = true;
+            PlayerPanel.SetActive(false);
+            PlayerChimeraPanel.SetActive(true);
+            CommandSelect = false;
+            currentState = GameState.PlayerChimeraSelect;
         }
-        if (IsGameOver)
+    }
+
+    public void PlayerChimeraSelect()
+    {
+        if (CommandSelect == true)
         {
-            ViewResult();
-            return;
+            PlayerPanel.SetActive(false);
+            PlayerChimeraPanel.SetActive(false);
+            CommandSelect = false;
+            currentState = GameState.PlayerTurnEnd;
         }
-        if (!IsPlayerTurn)
+    }
+
+
+
+    public void PlayerTurnEnd()
+    {
+        second += Time.deltaTime;
+        if (second >= 0.5f)
         {
-            second += Time.deltaTime;
-            if(second >= 1f)
-            {
-                second = 0f;
-                IsPlayerTurn = true;
-                player.OnDamage(enemy.at);
-            }
+            second = 0f;
+            currentState = GameState.EnemyChimeraSelect;
+        }
+
+    }
+
+    public void EnemySelect()
+    {
+
+    }
+
+    public void EnemyChimeraSelect()
+    {
+        player.OnDamage(enemy.at);
+        currentState = GameState.EnemyTurnEnd;
+    }
+
+    public void EnemyTurnEnd()
+    {
+        second += Time.deltaTime;
+        if (second >= 0.5f)
+        {
+            second = 0f;
+            PlayerPanel.SetActive(true);
+            PlayerChimeraPanel.SetActive(false);
+            currentState = GameState.PlayerSelect;
         }
         
+        
+    }
+
+    public void GameWin()
+    {
+
+    }
+
+    public void GameOver()
+    {
+
+    }
+
+    // 各コマンド
+    public void PushInstructionButton()
+    {
+        CommandSelect = true;
     }
 
     public void PushAttackButton()
     {
-        if (IsPlayerTurn)
-        {
-            enemy.OnDamage(player.at);
-            IsPlayerTurn = false;
-        }
+        enemy.OnDamage(player.at);
+        CommandSelect = true;
+        
     }
+
+    void Update()
+    {
+        switch (currentState)
+        {
+            case GameState.PlayerSelect:
+                PlayerSelect();
+                break;
+            case GameState.PlayerChimeraSelect:
+                PlayerChimeraSelect();
+                break;
+            case GameState.PlayerTurnEnd:
+                PlayerTurnEnd();
+                break;
+            case GameState.EnemySelect:
+                EnemySelect();
+                break;
+            case GameState.EnemyChimeraSelect:
+                EnemyChimeraSelect();
+                break;
+            case GameState.EnemyTurnEnd:
+                EnemyTurnEnd();
+                break;
+            case GameState.GameWin:
+                GameWin();
+                break;
+            case GameState.GameOver:
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    
 }
